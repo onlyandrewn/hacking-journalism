23f7dcd881ebe31c26624585396d43deee029235
Andrew Nguyen <info@andrewnguyen.ca>
2015-01-17 19:52:34 -0500

    Some JS problems

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,44 @@
-document.addEventListener( "DOMContentLoaded", function() {
-   var popcorn = Popcorn( "video" );
- });
+// document.addEventListener( "DOMContentLoaded", function() {
+// 	var pop = Popcorn( "video" );
+
+	// var targetText = $(document);
+
+// 	targetText.click(function() {
+// 		console.log('Hey, is this working?')
+// 		if (videoPlaying == false) {
+// 			console.log('test2')
+// 			fadeIn();
+// 		} else {
+// 			fadeOut();
+// 		}
+// 	});
+
+// 	function fadeIn() {
+// 		pop.play();
+// 	// video.loop = true;
+// 	pop.fadeTo(5,.5);
+// }
+
+// 	function fadeOut() {
+// 	pop.fadeTo(5,0);
+// 	pop.pause();
+// }
+// }, false);
+
+
+$(function(){
+	var videoPlaying = false;
+
+	$('button').click(function(){
+		console.log('Is this working?')
+	})
+});
+
+// 		if (videoPlaying == false) {
+// 			console.log('How about now?')
+// 			// fadeIn();
+// 		} else {
+// 			console.log('Nowwwww?')
+// 			// fadeOut();
+// 		}
+// });
